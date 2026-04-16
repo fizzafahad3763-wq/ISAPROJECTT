be99988942557fd3e138b6c4ef1e6619d2ce2afb
ac24aci <ac24aci@herts.ac.uk>
2026-04-17 00:45:57 +0100

    Book Class

--- src/test/java/isa/BookTest.java
+++ src/test/java/isa/BookTest.java
@@ -17,8 +17,6 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class BookTest {
     
-    public BookTest() {
-    }
     
     @BeforeAll
     public static void setUpClass() {
@@ -41,4 +39,58 @@ public class BookTest {
     //
     // @Test
     // public void hello() {}
+
+
+    @Test
+    public void testGetAuthor() {
+        System.out.println("This is the getAuthor test.");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "French", "9782709636346");
+        String pred_outcome = "Dan Brown";
+        String real_outcome = info.getAuthor();
+        assertEquals(pred_outcome, real_outcome);
+    }
+
+
+    @Test
+    public void testSetAuthor() {
+        System.out.println("This is the setAuthor testing.");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "French", "9782709636346");
+        String new_author = "Sally Bob";
+        info.setAuthor(new_author);
+        assertEquals(new_author, info.getAuthor());
+    }
+
+
+    @Test
+    public void testGetIsbn() {
+        System.out.println("This is the getIsbn test.");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "9782709636346", "French");
+        String pred_outcome = "9782709636346";
+        String real_outcome = info.getIsbn();
+        assertEquals(pred_outcome, real_outcome);
+    }
+
+    @Test
+    public void testSetIsbn() {
+        System.out.println("This is the setIsbn testing.");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "French", "9782709636346");
+        String new_ISBN = "1234567891234";
+        info.setIsbn(new_ISBN);
+        assertEquals(new_ISBN, info.getIsbn());
+    }
+
+
+   @Test
+    public void testToString() {
+        System.out.println("This is the toString testing.");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "9782709636346", "French");
+        String pred_outcome = "Book(Title: Le Symbole Perdu, Author: Dan Brown, ISBN: 9782709636346, Language: French)";
+        String real_outcome = info.toString();
+        assertEquals(pred_outcome, real_outcome);
+    }
 }
--- src/test/java/isa/ItemTest.java
+++ src/test/java/isa/ItemTest.java
@@ -41,4 +41,101 @@ public class ItemTest {
     //
     // @Test
     // public void hello() {}
+
+    /**
+     * Test of getTitle method, of class Item.
+     */
+    @Test
+    public void testGetTitle() {
+        System.out.println("This is the getTitle test.");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "9782709636346", "French");
+        String pred_outcome = "Le Symbole Perdu";
+        String real_outcome = info.getTitle();
+        assertEquals(pred_outcome, real_outcome);
+    }
+
+    @Test
+    public void testSetTitle() {
+        System.out.println("This is the setTitle test.");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "9782709636346", "French");
+        String new_title = "Java101";
+        info.setTitle(new_title);
+        assertEquals(new_title, info.getTitle());
+    }
+
+    @Test
+    public void testGetLanguage() {
+        System.out.println("This is the getLanguage test.");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "9782709636346", "French");
+        String pred_outcome = "French";
+        String real_outcome = info.getLanguage();
+        assertEquals(pred_outcome, real_outcome);
+    }
+
+    @Test
+    public void testSetLanguage() {
+        System.out.println("This is the setLanguage test.");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "9782709636346", "French");
+        String new_language = "English";
+        info.setLanguage(new_language); 
+        assertEquals(new_language, info.getLanguage());
+    }
+
+    @Test
+    public void testGetDonator() {
+        System.out.println("This is the getDonator test.");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "9782709636346", "French");
+        Member real_outcome = info.getDonator(); 
+        assertEquals(member, real_outcome);
+    }
+
+    @Test
+    public void testClearDonator() {
+        System.out.println("clearDonator");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "9782709636346", "French");
+        info.clearDonator();
+        assertNull(info.getDonator()); 
+    }
+
+    @Test
+    public void testLoanTo() {
+        System.out.println("This is the loanTo test");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Member borrower = new Member("Amy Smigh", "74 Main Street, Hertford, SG13 7AB", "a.smith@yahoo.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "9782709636346", "French");
+        info.loanTo(borrower);
+        assertFalse(info.isAvailable()); 
+    }
+
+    @Test
+    public void testIsAvailable() {
+        System.out.println("This is the isAvailable test");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "9782709636346", "French");
+        boolean result = info.isAvailable();
+        assertTrue(result); 
+    }
+
+    @Test
+    public void testReturnLoan() {
+        System.out.println("This is the returnLoan test");
+        Member member = new Member("John Wong", "1 Church Lane Hatfield, AL10 OAG", "wong.j@aol.com", 3);
+        Member borrower = new Member("Amy Smigh", "74 Main Street, Hertford, SG13 7AB", "a.smith@yahoo.com", 3);
+        Book info = new Book("Le Symbole Perdu", "Dan Brown", member, "9782709636346", "French");
+        info.loanTo(borrower);
+        info.returnLoan();
+        assertTrue(info.isAvailable()); 
+    }
+
+    public class ItemImpl extends Item {
+        public ItemImpl() {
+            super("", "", null);
+        }
+    }
 }
