f6581beb9084709b10ecd06fba7debf1bd9f92c0
sf24aap <sf24aap@herts.ac.uk>
2026-04-17 09:12:42 +0100

    collection class

--- src/main/java/isa/Collection.java
+++ src/main/java/isa/Collection.java
@@ -8,109 +8,82 @@ package isa;
  *
  * @author saniafarooq
  */
+
+
 import java.util.ArrayList;
 
-
-public class Collection {
+public class Collection 
+{
     
-    private ArrayList<Item> items;
-    private ArrayList<Member> members;
-  
-    public Collection() {
-        items = new ArrayList<>();
-        members = new ArrayList<>();
+    private ArrayList>item> itemList;
+    
+    public Collection()
+    {
+        itemList = new ArrayList<>();
     }
     
-   
-    public void  addBook(String title, String language, String author, String isbn, Member donator)
+    public void addBook(String title, String author, Member donator, String language, String isbn)
     {
-    Book book = new Book(title, author, donator, language, isbn);
-    items.add(book);
-    if (donator !=null){
-    donator.donateItem();
-}
-    
-}
-
-public void addDVD(String title, String language, String director,Member donator, String [] audioLanguages)
-{
-DVD dvd = new DVD(title, director, donator, language, audioLanguages);
-items.add(dvd);
-if(donator != null)
-{
-donator.donateItem();
-}
-}
-
-
-public ArrayList<Item> searchItems(String searchTerm){
-ArrayList<Item> result = new ArrayList<>();
- for(Item item : items){
-if(item.getTitle().toLowerCase().contains(searchTerm.toLowerCase()))
-{
-result.add(item);
-}
-}
-return result;
-}
-
-public Item getItem(String title)
-{
-for(Item item : items)
-{
-if(item.getTitle().equalsIgnoreCase(title)){
-return item;
-}
-}
-return null;
-}
-
-public void removeItem(Item item)
-{
-items.remove(item);
-}
-
-public void addMember(Member member)
-{
-    members.add(member);
-}
-public void removeMember(Member member)
-{
-    members.remove(member);
-}
-public ArrayList<Member> searchMembers(String searchTerm)
-{
-   ArrayList<Member> result = new ArrayList<>();
-   
-   for (Member member : members)
-   {
-       if (member.getName().toLowerCase().contains(searchTerm.toLowerCase()))
-       {
-           result.add(member);
-       }
-   }
-   
-   return result;
-}
-public Member getMemberByEmail(String email) 
-{
-    for (Member member : members)
-    {
-        if (member.getEmail().equalsIgnoreCase(email))
+        if(getItem(title) != null)
         {
-            return member;
+            System.out.println("Item is Already Exists");
+            return;
+        }
+        
+        Book book = new Book(title, author, donator, language, isbn);
+        itemList.add(book);
+        
+        if(donator != null)
+        {
+            donator.addDonation(book);
         }
     }
-    return null;
-}
-
-public ArrayList<Item> getItems()
-{
-    return items;
-}
-
-public ArrayList<Member> getMembers()
-{
-    return members;
-}
+    
+    public coid addDVD(String title, String director, Member donator, String language, String[] audioLanguages)
+    {
+        DVD dvd = new DVD(title, director, donator, language, audioLanguages);
+        itemList.add(dvd);
+        
+        if(donator != null)
+        {
+            donator.addDonation(dvd);
+        }
+    }
+    
+    public ArrayList<Item> searchItems(String searchTerm)
+    {
+        ArrayList<Item> results = new ArrayList<>();
+        
+        for (Item item : itemList)
+        {
+            if(item.getTitle() != null & item.getTitle().toLowerCase().contains(searchTerm.toLowerCase()))
+            {
+                results.add(item);
+            }
+        }
+        return results;
+    }
+    
+    public Item getItem(String title)
+    {
+        for(Item item : itemList)
+        {
+            if(item.getTitle().equalsIgnoreCase(title))
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+    public void removeItem(Item item)
+    {
+        itemList.remove(item);
+    }
+    
+    public ArrayList<Item> getItems()
+    {
+        return itemList;
+    }
+    
+    
 }
